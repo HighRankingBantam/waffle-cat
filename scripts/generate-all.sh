c48b1b4f099e5f689e57cd5297e516d95780b6ef
John Stuart Brown <jsbrown7@gmail.com>
2025-12-29 18:28:06 -0800

    generate all script added

--- scripts/generate-all.sh
+++ scripts/generate-all.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-root_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+source_path="${BASH_SOURCE[0]}"
+while [ -L "$source_path" ]; do
+  source_dir="$(cd "$(dirname "$source_path")" && pwd -P)"
+  source_path="$(readlink "$source_path")"
+  if [[ "$source_path" != /* ]]; then
+    source_path="$source_dir/$source_path"
+  fi
+done
+script_dir="$(cd "$(dirname "$source_path")" && pwd -P)"
+root_dir="$(cd "$script_dir/.." && pwd -P)"
 
 "$root_dir/scripts/generate-alacritty.sh"
 "$root_dir/scripts/generate-kitty.sh"
